9278a337182315bb98899325e4d312ceaeb3d762
Theano Zacharouli <61084287+theanoza@users.noreply.github.com>
2026-08-30 12:08:30 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.use(cookieParser('FoodieliciousSecure'));
+app.use(session({
+    secret: 'FoodieliciousSecretSession',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(flash());
+app.use(fileUpload());
+
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+const routes = require('./server/routes/recipe_routes.js');
+app.use('/', routes);
+
+app.listen(PORT, () => console.log('Server running on port:' ,PORT));
